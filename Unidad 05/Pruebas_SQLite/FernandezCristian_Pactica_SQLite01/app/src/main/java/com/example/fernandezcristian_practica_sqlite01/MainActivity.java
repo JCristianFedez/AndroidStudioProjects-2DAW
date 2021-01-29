9131030b34d864426bda4a96082bd80bc9f79eb2
JCristianFedez <73657780+JCristianFedez@users.noreply.github.com>
2021-01-29 16:27:17 +0100

    Si faltan datos por introducir no va al formulario

--- Unidad 05/Pruebas_SQLite/FernandezCristian_Pactica_SQLite01/app/src/main/java/com/example/fernandezcristian_practica_sqlite01/MainActivity.java
+++ Unidad 05/Pruebas_SQLite/FernandezCristian_Pactica_SQLite01/app/src/main/java/com/example/fernandezcristian_practica_sqlite01/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
@@ -44,27 +45,56 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         Intent intent = new Intent(this, AndroidBaseDatos.class);
-        String action="none";
-        String code=etCode.getText().toString();
-        String name=etName.getText().toString();
+        String action = "none";
+        String code = etCode.getText().toString();
+        String name = etName.getText().toString();
+        String datosFaltantes = "";
+
         switch (v.getId()) {
             case R.id.btnInsertar:
-                action="insert";
+                action = "insert";
+                if (code.length() <= 0 && name.length() <= 0) {
+                    datosFaltantes = "Falto introducir nombre y codigo";
+                } else if (code.length() <= 0) {
+                    datosFaltantes = "Falto introducir codigo";
+                } else if (name.length() <= 0) {
+                    datosFaltantes = "Falto introducir nombre";
+                }
                 break;
 
             case R.id.btnAct:
-                action="upload";
+                action = "upload";
+                if (code.length() <= 0 && name.length() <= 0) {
+                    datosFaltantes = "Falto introducir nombre y codigo";
+                } else if (code.length() <= 0) {
+                    datosFaltantes = "Falto introducir codigo";
+                } else if (name.length() <= 0) {
+                    datosFaltantes = "Falto introducir nombre";
+                }
                 break;
 
             case R.id.btnDel:
-                action="delete";
+                action = "delete";
+                if (code.length() <= 0) {
+                    datosFaltantes = "Debe introducir codigo del alumno a eliminar";
+                }
+
                 break;
             case R.id.btnConsulta:
-                action="buscar";
+                action = "buscar";
         }
-        intent.putExtra("action",action);
-        intent.putExtra("code",code);
-        intent.putExtra("name",name);
-        startActivity(intent);
+
+
+
+        //Envio a la siguiente actividad o muestro datos faltantes
+        if (datosFaltantes == "") {
+            intent.putExtra("action", action);
+            intent.putExtra("code", code);
+            intent.putExtra("name", name);
+            startActivity(intent);
+        } else {
+            Toast.makeText(getApplicationContext(), datosFaltantes, Toast.LENGTH_SHORT).show();
+
+        }
     }
 }
